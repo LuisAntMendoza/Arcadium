e6614d170d104c58d1751fa2a4bfab77cbfd55e1
Carlos-V24 <vihcprepa6@comunidad.unam.mx>
2020-06-13 11:29:27 -0500

    FEAT: Se añadio el juego Tetris

---
+++ dynamics/Tetris/Funciones_Tetris.js
@@ -0,0 +1,372 @@
+const COLUMNAS = 10;
+const FILAS = 20;
+var FilAct=0;
+var CeldAct=4;
+var RotarFig=0;
+var TetActual=Math.floor(Math.random()*Tetranomios.length);
+var TetProx=Math.floor(Math.random()*Tetranomios.length);
+var Tablero =document.getElementById('Tablero')
+var Proxima_Figura= document.getElementById("Proxima_Figura")
+///////////////////////////////////////////////////////////////////////////////
+//////*******  Generacion del terreno de juego y prox Tet  **********//////////
+///////////////////////////////////////////////////////////////////////////////
+
+for (var i = 0; i < FILAS; i++) {
+  var Fila= document.createElement("div")
+  Fila.classList.add("Columna")
+  Tablero.appendChild(Fila);
+  for (var n = 0; n < COLUMNAS; n++) {
+    var Celda =  document.createElement("div");
+    Celda.classList.add("celda");
+    Tablero.children[i].appendChild(Celda);
+  }
+}
+////////////////////////////  Proxima Figura  ///////////////////////////////////
+for (var i = 0; i < 6; i++) {
+  var Fila= document.createElement("div")
+  Fila.classList.add("Columna")
+  Proxima_Figura.appendChild(Fila);
+  for (var n = 0; n < 6; n++) {
+    var Celda =  document.createElement("div");
+    Celda.classList.add("celda");
+    Proxima_Figura.children[i].appendChild(Celda);
+  }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//////////**** Funciones de dibujado de las piezas en la pantalla ******///////
+///////////////////////////////////////////////////////////////////////////////
+
+function DibujarTet(FilActual, CeldaActual, Figura) {
+  let FigX=0;
+  let FigY=0;
+  for (var i = FilActual; i < FilActual+(Tetranomios[TetActual][Figura].length); i++) {
+    FigX=0
+    for (var n = CeldaActual; n < CeldaActual+Tetranomios[TetActual][Figura][FigY].length; n++) {
+      if (Tetranomios[TetActual][Figura][FigY][FigX]>0) {
+        Tablero.children[i].children[n].classList.add(TetColor[TetActual])
+      }
+      FigX++;
+    }
+    FigY++;
+  }
+}
+////////////////////////////////////////////////////////////////////////////////
+function DesDibujarTet(FilActual, CeldaActual, Figura) {
+  let FigX=0;
+  let FigY=0;
+  for (var i = FilActual; i < FilActual+(Tetranomios[TetActual][Figura].length); i++) {
+    FigX=0
+    for (var n = CeldaActual; n < CeldaActual+Tetranomios[TetActual][Figura][FigY].length; n++) {
+      if (Tetranomios[TetActual][Figura][FigY][FigX]>0){
+        Tablero.children[i].children[n].classList.remove(TetColor[TetActual])
+      }
+      FigX++;
+    }
+    FigY++;
+  }
+}
+////////////////////////////////////////////////////////////////////////////////
+function DibujarNext() {
+  let FigX=0;
+  let FigY=0;
+  for (var i = 1; i < Tetranomios[TetProx][0].length+1; i++) {
+    FigX=0
+    for (var n = 1; n < Tetranomios[TetProx][0][FigY].length+1; n++) {
+      if (Tetranomios[TetProx][0][FigY][FigX]>0) {
+        Proxima_Figura.children[i].children[n].classList.add(TetColor[TetProx])
+      }
+      FigX++;
+    }
+    FigY++;
+  }
+}
+////////////////////////////////////////////////////////////////////////////////
+function DesDibujarNext() {
+  let FigX=0;
+  let FigY=0;
+  for (var i = 1; i < Tetranomios[TetProx][0].length+1; i++) {
+    FigX=0
+    for (var n = 1; n < Tetranomios[TetProx][0][FigY].length+1; n++) {
+      if (Tetranomios[TetProx][0][FigY][FigX]>0) {
+        Proxima_Figura.children[i].children[n].classList.remove(TetColor[TetProx])
+      }
+      FigX++;
+    }
+    FigY++;
+  }
+}
+////////////////////////////////////////////////////////////////////////////////
+function GenerarTet() {
+  DesDibujarNext()
+  FilAct=0;
+  CeldAct=4;
+  RotarFig=0;
+  TetActual=TetProx;
+  TetProx=Math.floor(Math.random()*Tetranomios.length);
+  DibujarTet(FilAct, CeldAct, RotarFig)
+  DibujarNext()
+}
+///////////////////////////////////////////////////////////////////////////////
+/////////////******   Funcion del ovimiento del juego   ******/////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+//////////**** Funciones colisiones de las piezas en el juego ******///////////
+///////////////////////////////////////////////////////////////////////////////
+
+function ColParedIzq(PosFigura) {
+  let ColVacia=true;
+  let n = 0;
+  do {
+    for (let i = 0; i < Tetranomios[TetActual][PosFigura].length; i++) {
+      if (Tetranomios[TetActual][PosFigura][i][n]==0) {
+      }else {
+        ColVacia=false;
+      }
+    }
+    n++;
+  } while (ColVacia==true);
+  return n-1
+}
+////////////////////////////////////////////////////////////////////////////////
+function ColParedDer(PosFigura) {
+  let ColVacia=true;
+  let n = 0;
+  do {
+    for (let i = 0; i < Tetranomios[TetActual][PosFigura].length; i++) {
+      if (Tetranomios[TetActual][PosFigura][i][(Tetranomios[TetActual][PosFigura][i].length)-n-1]==0) {
+      }else {
+        ColVacia=false;
+      }
+    }
+    n++;
+  } while (ColVacia==true);
+  return n-1
+}
+///////////////////////////////////////////////////////////////////////////////
+function ColAbajo(PosFigura) {
+  let ColVacia=true;
+  let n = 0;
+  let alturaPieca=Tetranomios[TetActual][PosFigura].length-1;
+  do {
+    for (let i = 0; i < Tetranomios[TetActual][PosFigura].length; i++) {
+      if (Tetranomios[TetActual][PosFigura][alturaPieca-n][i]==0) {
+      }else {
+        ColVacia=false;
+      }
+    }
+    n++;
+  } while (ColVacia==true);
+  return n-1
+}
+////////////////////////////////////////////////////////////////////////////////
+function PoderRotar() {
+  var SiPuedo=true
+  let alturaPieca=Tetranomios[TetActual][RotarFig].length-1
+  //Comprueba en caso de que estes del lado izquierdo
+  if (CeldAct<0) {
+    if (RotarFig==3){
+      if(!(ColParedIzq(RotarFig)<=ColParedIzq(0))){
+        SiPuedo= false;
+      }
+    }else {
+      if(!(ColParedIzq(RotarFig)<=ColParedIzq(RotarFig+1))){
+        SiPuedo= false;
+      }
+    }
+  }else if (CeldAct>COLUMNAS-4 && TetActual==5) {
+    //Caso especifico del tetranomio I
+    if (RotarFig%2==0) {
+      SiPuedo= false;
+    }
+  }else if (CeldAct>COLUMNAS-3) {
+    //Comprueba en caso de que estes del lado derecho
+    if (RotarFig==3){
+      if(ColParedDer(RotarFig)<=ColParedDer(0)){
+        SiPuedo= true;
+      }else {
+        SiPuedo= false;
+      }
+    }else {
+      if(ColParedDer(RotarFig)<=ColParedDer(RotarFig+1)){
+        SiPuedo= true;
+      }else {
+        SiPuedo= false;
+      }
+    }
+  }else if (FilAct+alturaPieca>FILAS-1) {
+    if (RotarFig==3){
+      if(ColAbajo(RotarFig)<=ColAbajo(0)){
+        SiPuedo= true;
+      }else {
+        SiPuedo= false;
+      }
+    }else {
+      if(ColAbajo(RotarFig)<=ColAbajo(RotarFig+1)){
+        SiPuedo= true;
+      }else {
+        SiPuedo= false;
+      }
+    }
+  }
+  return SiPuedo
+}
+////////////////////////////////////////////////////////////////////////////////
+function ColIzqTet(FilActual, CeldaActual, Figura) {
+    let FigX=0;
+    let FigY=0;
+    let x =ColParedIzq(RotarFig)
+    let y= ColParedDer(RotarFig)
+    let z= ColAbajo(RotarFig)
+    var SiPuedo=true;
+    for (var i = FilActual; i < FilActual+(Tetranomios[TetActual][Figura].length)-z; i++) {
+      FigX=0;
+      for (var n = CeldaActual-1; n < CeldaActual+Tetranomios[TetActual][Figura][FigY].length-x-1; n++) {
+        if (n>=0 && n<=9) {
+          if (Tablero.children[i].children[n].classList.length>1 && Tetranomios[TetActual][Figura][FigY][FigX]>0) {
+            SiPuedo=false;
+          }
+        }
+        FigX++;
+      }
+      FigY++;
+    }
+    return SiPuedo;
+}
+////////////////////////////////////////////////////////////////////////////////
+function ColDerTet(FilActual, CeldaActual, Figura) {
+    let FigX=0;
+    let FigY=0;
+    let x =ColParedIzq(RotarFig)
+    let y= ColParedDer(RotarFig)
+    let z= ColAbajo(RotarFig)
+    var SiPuedo=true;
+    for (var i = FilActual; i < FilActual+(Tetranomios[TetActual][Figura].length)-z; i++) {
+      FigX=0;
+      for (var n = CeldaActual+1; n < CeldaActual+Tetranomios[TetActual][Figura][FigY].length-y+1; n++) {
+        if (n>=0 && n<=9) {
+          if (Tablero.children[i].children[n].classList.length>1 && Tetranomios[TetActual][Figura][FigY][FigX]>0) {
+            SiPuedo=false;
+          }
+        }
+        FigX++;
+      }
+      FigY++;
+    }
+    return SiPuedo;
+}
+/////////////////////////////////////////////////////////////////////////////////
+function ColAbajoTet(FilActual, CeldaActual, Figura) {
+    let FigX=0;
+    let FigY=0;
+    let x =ColParedIzq(RotarFig)
+    let y= ColParedDer(RotarFig)
+    let z= ColAbajo(RotarFig)
+    var SiPuedo=true;
+    for (var i = FilActual+1; i < FilActual+1+(Tetranomios[TetActual][Figura].length)-z; i++) {
+      FigX=0;
+      for (var n = CeldaActual; n < CeldaActual+Tetranomios[TetActual][Figura][FigY].length; n++) {
+        if (n>=0 && n<=9) {
+          if (Tablero.children[i].children[n].classList.length>1 && Tetranomios[TetActual][Figura][FigY][FigX]>0) {
+            SiPuedo=false;
+          }
+        }
+        FigX++;
+      }
+      FigY++;
+    }
+    return SiPuedo;
+}
+////////////////////////////////////////////////////////////////////////////////
+function PoderRotTet(FilActual, CeldaActual, Figura) {
+  let FigX=0;
+  let FigY=0;
+  let x =ColParedIzq(RotarFig)
+  let y= ColParedDer(RotarFig)
+  let z= ColAbajo(RotarFig)
+  if (Figura==3) {
+    var NextFig=0;
+  }else {
+    var NextFig=Figura+1;
+  }
+  var SiPuedo=true;
+  for (var i = FilActual; i < FilActual+(Tetranomios[TetActual][NextFig].length); i++) {
+    FigX=0;
+    for (var n = CeldaActual; n < CeldaActual+Tetranomios[TetActual][NextFig][FigY].length; n++) {
+      if (n>=0 && n<=9) {
+        if (Tablero.children[i].children[n].classList.length>1 && Tetranomios[TetActual][NextFig][FigY][FigX]>0) {
+          SiPuedo=false;
+        }
+      }
+      FigX++;
+    }
+    FigY++;
+  }
+  return SiPuedo;
+}
+///////////////////////////////////////////////////////////////////////////////
+//////****** Funciones de destruir y crear las filas y puntuacion ******///////
+///////////////////////////////////////////////////////////////////////////////
+function FilasHechas() {
+  var FilasAremover=[];
+  for (var i = 0; i < FILAS; i++) {
+    var CeldasLlenas=0;
+    for (var n = 0; n < COLUMNAS; n++) {
+      if(Tablero.children[i].children[n].classList.length>1){
+        CeldasLlenas++;
+      }
+    }
+    if (CeldasLlenas==10) {
+      FilasAremover.push(i)
+    }
+  }
+  if (FilasAremover.length>0) {
+    if (FilasAremover.length==4) {
+      Make_Tetris.play()
+    }else if (FilasAremover.length==3) {
+      Make_Triple.play()
+    }else{
+      Destroy_Row.play()
+    }
+    Velocidad-=20;
+    Puntuacion+=Math.pow((FilasAremover.length)*10, 2)
+    FilasDestruidas+=FilasAremover.length
+    document.getElementById("FilasD").innerHTML = FilasDestruidas;
+    ModifcarPuntaje(Puntuacion);
+    for (var i = FilasAremover.length-1; i >=0 ; i--) {
+      Tablero.children[FilasAremover[i]].remove();
+    }
+    for (var i = FilasAremover.length-1; i >=0 ; i--) {
+      let Fila= document.createElement("div")
+      Fila.classList.add("Columna")
+      for (var n = 0; n < COLUMNAS; n++) {
+        let Celda =  document.createElement("div");
+        Celda.classList.add("celda");
+        Fila.appendChild(Celda);
+      }
+      Tablero.insertBefore(Fila, Tablero.firstChild);
+    }
+  }
+}
+////////////////////////////////////////////////////////////////////////////////////////
+  function ModifcarPuntaje(Puntuacion){
+    document.getElementById("Puntuacion").innerHTML = Puntuacion;
+  }
+
+  function SeguirVivo() {
+    for (var i = 0; i < 4; i++) {
+      for (var n = 0; n < COLUMNAS; n++) {
+        if (Tablero.children[i].children[n].classList.length>2) {
+          Vivo=false;
+        }
+      }
+    }
+    if (!Vivo) {
+      Musica_Fondo_1.pause()
+      setTimeout(()=>{
+        Game_Over.play()
+      }, 200)
+    }
+    return Vivo;
+  }
---
+++ dynamics/Tetris/Tetris.js
@@ -0,0 +1,133 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+///////*********************  Inico Tetris by Carlos-V24 :D *************************///////
+////////////////////////////////////////////////////////////////////////////////////////////
+
+//Primer Tetranomio que aparece
+DibujarTet(FilAct, CeldAct, RotarFig)
+DibujarNext()
+var Velocidad = 1100;
+var Puntuacion = 0;
+
+let Musica_Fondo_1 = new Audio();
+let Make_Triple = new Audio();
+let Make_Tetris = new Audio();
+let Destroy_Row = new Audio();
+let Rotar = new Audio();
+let Fijar = new Audio();
+let Mover = new Audio();
+let Game_Over = new Audio();
+
+Mover.src = "../statics/Tetris/Sonidos/Mover.mp3";
+Game_Over.src = "../statics/Tetris/Sonidos/Muerto.mp3";
+Fijar.src = "../statics/Tetris/Sonidos/Fijar.mp3";
+Rotar.src = "../statics/Tetris/Sonidos/Rotar.mp3";
+Musica_Fondo_1.src = "../statics/Tetris/Sonidos/Tetris_99.mp3";
+Make_Triple.src = "../statics/Tetris/Sonidos/Make_Triple.mp3";
+Make_Tetris.src = "../statics/Tetris/Sonidos/Hacer_tetris.mp3";
+Destroy_Row.src = "../statics/Tetris/Sonidos/Destruir_fila.mp3";
+Musica_Fondo_1.loop = true;
+var FilasDestruidas = 0;
+var Vivo=true;
+var MusicaON = true;
+//Estado Dela Musica{
+document.getElementById("Musica").addEventListener("click",()=>{
+  if (document.getElementById("Musica").classList.contains("Si")) {
+    Musica_Fondo_1.pause()
+    MusicaON=false;
+    document.getElementById("Musica").classList.remove("Si")
+    document.getElementById("Musica").classList.add("No")
+  }else if (document.getElementById("Musica").classList.contains("No")) {
+    if (Vivo) {
+      Musica_Fondo_1.play()
+    }
+    MusicaON=true;
+    document.getElementById("Musica").classList.remove("No")
+    document.getElementById("Musica").classList.add("Si")
+  }
+})
+//Movimiento de los tetranomios
+document.addEventListener("keydown",()=>{
+  if (Vivo) {
+    let dir = event.keyCode;
+    if (MusicaON) {
+      Musica_Fondo_1.play()
+    }
+    //Movimiento Hacia Arriba
+    if( dir == 37 &&  CeldAct>0-ColParedIzq(RotarFig)){
+      DesDibujarTet(FilAct, CeldAct, RotarFig);
+      Mover.play()
+      if(ColIzqTet(FilAct, CeldAct, RotarFig)){
+        CeldAct--;
+      }
+      DibujarTet(FilAct, CeldAct, RotarFig);
+      Fijar.play()
+      FilasHechas()
+    }else if(dir == 39 && (CeldAct+Tetranomios[TetActual][RotarFig][0].length)-ColParedDer(RotarFig)<COLUMNAS){
+      DesDibujarTet(FilAct, CeldAct, RotarFig);
+      Mover.play()
+      if(ColDerTet(FilAct, CeldAct, RotarFig)){
+        CeldAct++;
+      }
+      DibujarTet(FilAct, CeldAct, RotarFig);
+    }else if(dir == 40 && FilAct-ColAbajo(RotarFig)<(FILAS-Tetranomios[TetActual][RotarFig].length)){//Este es abajo
+      Puntuacion+=1
+      DesDibujarTet(FilAct, CeldAct, RotarFig);
+      if(!(ColAbajoTet(FilAct, CeldAct, RotarFig))){
+        DibujarTet(FilAct, CeldAct, RotarFig)
+        Fijar.play()
+        FilasHechas()
+
+        GenerarTet()
+      }else if (FilAct-ColAbajo(RotarFig)+Tetranomios[TetActual][RotarFig].length==19) {
+        FilAct++;
+        DibujarTet(FilAct, CeldAct, RotarFig)
+        Fijar.play()
+        FilasHechas()
+        GenerarTet()
+      }else {
+        FilAct++;
+      }
+      DibujarTet(FilAct, CeldAct, RotarFig)
+    }else if(dir == 38 && PoderRotar()){
+      Rotar.play()
+      DesDibujarTet(FilAct, CeldAct, RotarFig);
+      if (PoderRotTet(FilAct, CeldAct, RotarFig)) {
+        if (RotarFig==3) {
+          RotarFig=0;
+        }else {
+          RotarFig++;
+        }
+      }
+      DibujarTet(FilAct, CeldAct, RotarFig)
+    }
+  }
+});
+function Tetris(Velocidad) {
+  setTimeout(()=>{
+    if(FilAct-ColAbajo(RotarFig)<(FILAS-Tetranomios[TetActual][RotarFig].length)){
+      DesDibujarTet(FilAct, CeldAct, RotarFig);
+      if(!(ColAbajoTet(FilAct, CeldAct, RotarFig))){
+        DibujarTet(FilAct, CeldAct, RotarFig)
+        Fijar.play()
+        FilasHechas()
+        GenerarTet()
+      }else if (FilAct-ColAbajo(RotarFig)+Tetranomios[TetActual][RotarFig].length==19) {
+        FilAct++;
+        DibujarTet(FilAct, CeldAct, RotarFig)
+        Fijar.play()
+        FilasHechas()
+        GenerarTet()
+      }else {
+        FilAct++;
+        DibujarTet(FilAct, CeldAct, RotarFig)
+      }
+      SeguirVivo()
+      if (SeguirVivo()) {
+        Tetris(Velocidad)
+      }else {
+        console.log("Muero");
+      }
+    }
+  },Velocidad)
+}
+Tetris(Velocidad);
