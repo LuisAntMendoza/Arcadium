6cf7b3d37d7021db28067cb33911d82697685036
Carlos-V24 <vihcprepa6@comunidad.unam.mx>
2020-06-15 17:01:00 -0500

    FEAT: Se añadió la barra de navegación

---
+++ dynamics/Barra_navegacion.js
@@ -0,0 +1,15 @@
+$("#Inicio").click(()=>{
+  console.log("Enviar al Inicio");
+});
+
+$("#Juegos").click(()=>{
+  console.log("Muestra todos los juegos");
+});
+
+$("#Puntajes").click(()=>{
+  console.log("Enviar al los maximos puntajes");
+});
+
+$("#Creditos").click(()=>{
+  console.log("Mostar creditos enmedio de la pantalla con modal");
+});
