5b1e756c455ebcbe27ee95e645946454750a29b0
Carlos-V24 <vihcprepa6@comunidad.unam.mx>
2020-06-18 21:01:27 -0500

    FEAT: Se agregaron algunos wireframes de la pagina

--- Index.js
+++ Index.js
@@ -1,1 +1,5 @@
 window.location ="templates/Inicio.html"
+/*Se opto por redirigir a la la pagina templates/Inicio.html,
+debido a que por cuestiones practicas y tecnias todos los rachivos
+html de la pagina usan rutas relativas en la barra de Barra_navegacion y
+y para reciclar codigo es mas praxtico solo redireccionar en vez de hacer una especial para el inicio aqui*/
