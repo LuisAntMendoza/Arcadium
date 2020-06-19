f5ed9941f41fba24557097d80a718f4f9f6312ab
LuisAntMendoza <mendozaluis@comunidad.unam.mx>
2020-06-18 23:19:34 -0500

    FEAT: se agregaron las funciones para agregar los mejores puntajes.

--- dynamics/js/Puntajes.js
+++ dynamics/js/Puntajes.js
@@ -31,3 +31,108 @@ function valCookie(nombre) {
     }
     return regreso;
 }
+
+function llenarBuscaminas() {
+    let cookie = valCookie("scoresBuscaminas");
+    let cookieSplit = cookie.split(",");
+    let arrScores = [];
+    for (let i = 0; i < (cookieSplit.length / 3); i++) {
+        arrScores[i] = cookieSplit[i * 3];
+    }
+    arrScores.sort((a, b) => {
+        return a - b;
+    })
+    let arrScoresSort = [];
+    for (var i = 0; i < 5; i++) {
+        if (arrScores.length != 0) {
+            arrScoresSort[i] = arrScores.pop();
+        }
+    }
+    for (var i = 0; i < arrScoresSort.length; i++) {
+        if (arrScoresSort.length != 0) {
+            let indice = cookieSplit.indexOf(arrScoresSort[i]);
+            let fila = $("<tr>");
+            let jugador = $("<td>");
+            $(jugador).text(cookieSplit[indice + 1]);
+            let puntuacion = $("<td>");
+            $(puntuacion).text(cookieSplit[indice]);
+            let tiempo = $("<td>");
+            let valTiempo = new Date(parseInt(cookieSplit[indice + 2]));
+            $(tiempo).text(valTiempo.toLocaleString());
+            $(fila).append(jugador, puntuacion, tiempo);
+            $("#body-Buscaminas").append(fila);
+        }
+    }
+}
+
+function llenarTetris() {
+    let cookie = valCookie("scoresTetris");
+    let cookieSplit = cookie.split(",");
+    let arrScores = [];
+    for (let i = 0; i < (cookieSplit.length / 3); i++) {
+        arrScores[i] = cookieSplit[i * 3];
+    }
+    arrScores.sort((a, b) => {
+        return a - b;
+    })
+    let arrScoresSort = [];
+    for (var i = 0; i < 5; i++) {
+        if (arrScores.length != 0) {
+            arrScoresSort[i] = arrScores.pop();
+        }
+    }
+    for (var i = 0; i < arrScoresSort.length; i++) {
+        if (arrScoresSort.length != 0) {
+            let indice = cookieSplit.indexOf(arrScoresSort[i]);
+            let fila = $("<tr>");
+            let jugador = $("<td>");
+            $(jugador).text(cookieSplit[indice + 1]);
+            let puntuacion = $("<td>");
+            $(puntuacion).text(cookieSplit[indice]);
+            let tiempo = $("<td>");
+            let valTiempo = new Date(parseInt(cookieSplit[indice + 2]));
+            $(tiempo).text(valTiempo.toLocaleString());
+            $(fila).append(jugador, puntuacion, tiempo);
+            $("#body-Tetris").append(fila);
+        }
+    }
+}
+
+function llenarSpace() {
+    let cookie = valCookie("scoresSpace");
+    let cookieSplit = cookie.split(",");
+    let arrScores = [];
+    for (let i = 0; i < (cookieSplit.length / 3); i++) {
+        arrScores[i] = cookieSplit[i * 3];
+    }
+    arrScores.sort((a, b) => {
+        return a - b;
+    })
+    let arrScoresSort = [];
+    for (var i = 0; i < 5; i++) {
+        if (arrScores.length != 0) {
+            arrScoresSort[i] = arrScores.pop();
+        }
+    }
+    for (var i = 0; i < arrScoresSort.length; i++) {
+        if (arrScoresSort.length != 0) {
+            let indice = cookieSplit.indexOf(arrScoresSort[i]);
+            let fila = $("<tr>");
+            let jugador = $("<td>");
+            $(jugador).text(cookieSplit[indice + 1]);
+            let puntuacion = $("<td>");
+            $(puntuacion).text(cookieSplit[indice]);
+            let tiempo = $("<td>");
+            let valTiempo = new Date(parseInt(cookieSplit[indice + 2]));
+            $(tiempo).text(valTiempo.toLocaleString());
+            $(fila).append(jugador, puntuacion, tiempo);
+            $("#body-Space").append(fila);
+        }
+    }
+}
+
+$(document).ready(() => {
+    llenarBuscaminas();
+    llenarTetris();
+    llenarSpace();
+})
